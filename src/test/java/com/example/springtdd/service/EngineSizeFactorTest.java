502f4964fb21c759654bb97aba645f9ee096749d
Anna Saweljew <anna.saweljew@o2.pl>
2022-02-06 15:40:19 +0100

    add EngineSizeFactorTest class with failed tests - red phase

---
+++ src/test/java/com/example/springtdd/service/EngineSizeFactorTest.java
@@ -0,0 +1,59 @@
+package com.example.springtdd.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class EngineSizeFactorTest {
+
+/*    The engine size factor is calculated as:
+             1000 factor is 1.0
+             1600 factor is 1.6
+             2000 factor is 2.0
+             2500 factor is 2.5
+             3000 factor is 3.0
+             Other factor is 3.5
+*/
+
+    @Test
+    @DisplayName("Should return factor 1.0 when engine size is 1000")
+    void shouldReturnFactor_WhenEngineSize1000(){
+        fail();
+    }
+
+    @Test
+    @DisplayName("Should return factor 1.6 when engine size is 1600")
+    void shouldReturnFactor_WhenEngineSize1600(){
+        fail();
+    }
+
+    @Test
+    @DisplayName("Should return factor 2.0 when engine size is 2000")
+    void shouldReturnFactor_WhenEngineSize2000(){
+        fail();
+    }
+
+    @Test
+    @DisplayName("Should return factor 2.5 when engine size is 2500")
+    void shouldReturnFactor_WhenEngineSize2500(){
+        fail();
+    }
+
+    @Test
+    @DisplayName("Should return factor 3.0 when engine size is 3000")
+    void shouldReturnFactor_WhenEngineSize3000(){
+        fail();
+    }
+
+    @Test
+    @DisplayName("Should return factor 3.5 when engine size is 3500")
+    void shouldReturnFactor_WhenEngineSize3500(){
+        fail();
+    }
+
+
+
+
+}
