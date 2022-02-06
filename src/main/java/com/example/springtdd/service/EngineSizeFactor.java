dc0b982afdc147e65748e2166b2dcbf5abff4b98
Anna Saweljew <anna.saweljew@o2.pl>
2022-02-06 15:54:40 +0100

    implement EngineSizeFactorTest methods and engineSizeFactor service method - green phase

--- src/main/java/com/example/springtdd/service/EngineSizeFactor.java
+++ src/main/java/com/example/springtdd/service/EngineSizeFactor.java
@@ -1,4 +1,23 @@
 package com.example.springtdd.service;
 
 public class EngineSizeFactor {
+
+    public double engineSizeFactor(int engineSize) {
+
+        switch (engineSize) {
+            case 1000:
+                return 1.0;
+            case 1600:
+                return 1.6;
+            case 2000:
+                return 2.0;
+            case 2500:
+                return 2.5;
+            case 3000:
+                return 3.0;
+            default:
+                return 3.5;
+        }
+    }
+
 }
--- src/test/java/com/example/springtdd/service/EngineSizeFactorTest.java
+++ src/test/java/com/example/springtdd/service/EngineSizeFactorTest.java
@@ -1,9 +1,8 @@
 package com.example.springtdd.service;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class EngineSizeFactorTest {
@@ -17,40 +16,62 @@ class EngineSizeFactorTest {
              Other factor is 3.5
 */
 
+    private final EngineSizeFactor engineSizeFactor = new EngineSizeFactor();
+
     @Test
     @DisplayName("Should return factor 1.0 when engine size is 1000")
     void shouldReturnFactor_WhenEngineSize1000(){
-        fail();
+        int engineSize = 1000;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(1.0, factor);
     }
 
     @Test
     @DisplayName("Should return factor 1.6 when engine size is 1600")
     void shouldReturnFactor_WhenEngineSize1600(){
-        fail();
+        int engineSize = 1600;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(1.6, factor);
     }
 
     @Test
     @DisplayName("Should return factor 2.0 when engine size is 2000")
     void shouldReturnFactor_WhenEngineSize2000(){
-        fail();
+        int engineSize = 2000;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(2.0, factor);
     }
 
     @Test
     @DisplayName("Should return factor 2.5 when engine size is 2500")
     void shouldReturnFactor_WhenEngineSize2500(){
-        fail();
+        int engineSize = 2500;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(2.5, factor);
     }
 
     @Test
     @DisplayName("Should return factor 3.0 when engine size is 3000")
     void shouldReturnFactor_WhenEngineSize3000(){
-        fail();
+        int engineSize = 3000;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(3.0, factor);
     }
 
     @Test
-    @DisplayName("Should return factor 3.5 when engine size is 3500")
+    @DisplayName("Should return factor 3.5 when engine size is 3700")
     void shouldReturnFactor_WhenEngineSize3500(){
-        fail();
+        int engineSize = 3700;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(3.5, factor);
+    }
+
+    @Test
+    @DisplayName("Should return factor 3.5 when engine size is 0")
+    void shouldReturnFactor_WhenEngineSizeNotProvided(){
+        int engineSize = 0;
+        double factor = engineSizeFactor.engineSizeFactor(engineSize);
+        Assertions.assertEquals(3.5, factor);
     }
 
 
